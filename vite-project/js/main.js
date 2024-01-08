6a9a9dedf1cb8a6ff26feadb82bee879573da0bd
AnthonyPvZ <anthonypvz123@gmail.com>
2024-01-07 23:12:35 -0500

    Squashed commit of the following:
    commit eab88d199454026ddf4a64404c8f40356744e7e6
    Author: AnthonyPvZ <anthonypvz123@gmail.com>
    Date:   Sun Jan 7 22:58:52 2024 -0500
    
        Style
    
    commit 6f5e216cfd2bcb0528d208d4cb26ddefed99de78
    Author: AnthonyPvZ <anthonypvz123@gmail.com>
    Date:   Fri Jan 5 09:18:58 2024 -0500
    
        some alt text
    
    commit b30c5e6d7f2b0fd8e38fc8e07d61b01fb9cad164
    Author: AnthonyPvZ <anthonypvz123@gmail.com>
    Date:   Thu Jan 4 09:20:05 2024 -0500
    
        caps unsensitive
    
    commit 62ec007071d5a4af96e099e919d44acfa49a1b9c
    Author: AnthonyPvZ <anthonypvz123@gmail.com>
    Date:   Thu Jan 4 03:01:01 2024 -0500
    
        memegenerated
    
    commit 5dd1b610619f5430e62d3062b191efbd4d97558c
    Author: AnthonyPvZ <anthonypvz123@gmail.com>
    Date:   Wed Jan 3 09:22:52 2024 -0500
    
        k
    
    commit bbd43cab22a2424dc1746bd0504ea5407b892d6f
    Author: AnthonyPvZ <anthonypvz123@gmail.com>
    Date:   Wed Jan 3 03:17:47 2024 -0500
    
        lk
    
    commit f26f909fb53572b8cbd835d649e6deaca74c57ec
    Author: AnthonyPvZ <anthonypvz123@gmail.com>
    Date:   Tue Jan 2 09:24:22 2024 -0500
    
        k
    
    commit fe05056ce09cdb76d4fcd3b7262d952678685b41
    Author: AnthonyPvZ <anthonypvz123@gmail.com>
    Date:   Fri Dec 22 03:15:55 2023 -0500
    
        Update main.js
    
    commit bc15c09ee78a9003300a8ca512b2920a7897b6d9
    Author: AnthonyPvZ <anthonypvz123@gmail.com>
    Date:   Thu Dec 21 09:10:31 2023 -0500
    
        Update main.js
    
    commit 8e0184cc6931bf0a3607bee5bb9497d5e1200f88
    Author: AnthonyPvZ <anthonypvz123@gmail.com>
    Date:   Wed Dec 20 09:23:43 2023 -0500
    
        Update main.js
    
    commit 1c9d0d05815c0b7f9e4b4b0eea0c80567d123811
    Author: AnthonyPvZ <anthonypvz123@gmail.com>
    Date:   Wed Dec 20 08:43:41 2023 -0500
    
        Update main.js
    
    commit 0870aedb6b907fc73156ac0fe2ac246315a0a303
    Author: AnthonyPvZ <anthonypvz123@gmail.com>
    Date:   Mon Dec 18 09:24:28 2023 -0500
    
        Update main.js
    
    commit f9a321de462f9fb74f2d6414dc8d4003dc7ceee1
    Author: AnthonyPvZ <anthonypvz123@gmail.com>
    Date:   Mon Dec 18 08:46:16 2023 -0500
    
        Update main.js
    
    commit 9b54e74a83418e2f31b092ca65779b40da5a6365
    Author: AnthonyPvZ <anthonypvz123@gmail.com>
    Date:   Tue Dec 12 09:22:16 2023 -0500
    
        nothin
    
    commit 7b550d498a1ac858d8bd579e4423bb8abc3624e2
    Author: AnthonyPvZ <anthonypvz123@gmail.com>
    Date:   Mon Dec 11 09:23:18 2023 -0500
    
        m
    
    commit 94ae297b96c23677dc7eeffa64390e6a35e31b02
    Author: AnthonyPvZ <anthonypvz123@gmail.com>
    Date:   Fri Dec 8 09:24:26 2023 -0500
    
        k

---
+++ vite-project/js/main.js
@@ -0,0 +1,93 @@
+import '../css/style.css'
+
+const URL = `https://api.imgflip.com/get_memes`;
+const filtermemes = async () => {
+    try{
+    container.innerHTML = ""
+    let meme = document.getElementById("input").value.toLowerCase();
+    const response = await fetch(URL);
+    const memeslist = await response.json();
+    if (response.status != 200) {
+        throw new Error(response.statusText)
+    }
+    let y = Array.from(memeslist.data.memes);
+    const foundmeme = y.filter(function (b) {
+        return b.name.toLowerCase().includes(meme)
+    })
+    console.log(foundmeme)
+    function create(blah) {
+        blah.map((result) => {
+            const card = document.createElement('div');
+            card.classlist = 'card'
+            const desc = `<div class=card><h1 class="card-title" alt="The meme is">Meme:</h1>
+            <h2 class="memename" alt="${result.name}">${result.name}</h2>
+            <img class=card-img alt="${result.name}" src="${result.url}"></div>`
+            container.innerHTML += desc;
+        })
+    }
+    create(foundmeme)
+}
+    catch (error){
+        console.log(error)
+    }
+}
+document.getElementById("query").onclick = filtermemes
+// async function getData(URL){
+//     try{
+//         const response = await fetch(URL);
+//         const data = await response.json();
+//         console.log(data);
+//         if (response.status != 200) {
+//             throw new Error(response.statusText)
+//         }
+//         let x = Array.from(data.data.memes);
+//         let meme = document.getElementById("meme").value;
+//         const TwoBtns = x.filter(function(b){
+//             return b.name.includes(meme)
+//         })
+//         console.log(TwoBtns)
+//         for(let i=0;i<x.length; i++){
+
+//             // console.log(x[i]);
+//         }
+//         console.log(response);
+//         document.getElementById("name").innerText === TwoBtns.name
+//     }
+//     catch (error){
+//         console.log(error)
+//     }
+// }
+// console.log(getData(URL));
+
+
+// const searchmemes = async(name)=>{
+//     const response = await fetch(URL)
+//     const data = await response.json();
+//     console.log(data)
+//     return data
+// }
+
+// const whenclicked = async()=>{
+//     let name = document.getElementById("meme").value
+//     console.log(name)
+//     let result = await searchmemes(name)
+//     let memes = result.data.memes.length > 0 ? result.data[0] : null
+//     console.log(memes)
+//     if (memes){
+//         document.getElementById("name").innerText = `Meme: ${memes["name"]}`
+//         document.getElementById("image").innerHTML = `URL: ${memes["url"]}`
+// }
+// }
+// document.getElementById("query").onclick = whenclicked
+// generate a card
+// const container = document.querySelector("#container")
+
+// function create(blah){
+//   blah.foreach((result)=>{
+//     const card = document.createElement('div');
+//     card.classlist = 'card'
+//     const desc = `<img src="${result.url}">`
+//     container.innerHTML = desc;
+//     })
+// }
+// create()
